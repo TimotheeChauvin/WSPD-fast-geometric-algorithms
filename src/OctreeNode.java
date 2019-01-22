f1923698ce45a8c27ca87f7d78686d747aaddf70
TimotheeChauvin <timothee.chauvin@orange.fr>
2019-01-22 14:12:46 +0100

    format, correct min_and_max_dim()

--- src/OctreeNode.java
+++ src/OctreeNode.java
@@ -10,9 +10,9 @@ import Jcg.geometry.Point_3;
  */
 public class OctreeNode {
 	public int level;
-	public OctreeNode[] children=null;
+	public OctreeNode[] children = null;
 	public OctreeNode father;
-	public Point_3 p; //point stored in a leaf
+	public Point_3 p; // point stored in a leaf
 
 	/**
 	 * Create the octree for storing an input point cloud
@@ -24,76 +24,82 @@ public class OctreeNode {
 		this.children = null;
 		this.father = null;
 		this.p = null;
-		for (Point_3 p : points){
+		for (Point_3 p : points) {
 			this.add(p, L, center);
 		}
 	}
-	
+
+	public OctreeNode(int level, OctreeNode father) {
+		this.level = level;
+		this.children = null;
+		this.father = father;
+	}
+
 	/**
 	 * Add a node into the OctreeNode
 	 */
 	public void add(Point_3 point, double L, Point_3 center) {
-		if (this.children == null){
-			if (this.p == null){
+		if (this.children == null) { // only one level
+			if (this.p == null) { // no point yet
 				this.p = point;
 			}
-			else{
-				this.children = new OctreeNode[8];
-				for (int i = 0; i<8; i++){
-					this.children[i].level = this.level +1;
+			else { // only one level, already one point
+				this.children = new OctreeNode(this.level+1, this)[8];
+				for (int i = 0; i < 8; i++) {
+					this.children[i].level = this.level + 1;
 				}
 			}
 		}
 	}
 
-	public static double[][] min_and_max_dim(List<Point_3> points){
+	public static double[][] min_and_max_dim(List<Point_3> points) {
 		double[][] min_max_dim = new double[2][3];
 		min_max_dim[0][0] = points.get(0).x;
 		min_max_dim[0][1] = points.get(0).y;
 		min_max_dim[0][2] = points.get(0).z;
-		
+
 		min_max_dim[1][0] = points.get(0).x;
 		min_max_dim[1][1] = points.get(0).y;
 		min_max_dim[1][2] = points.get(0).z;
-		for (Point_3 point : points){
-			if (point.x < min_max_dim[0][0]){
-				min_max_dim[1][0] = point.x;
+		for (Point_3 point : points) {
+			if (point.x < min_max_dim[0][0]) {
+				min_max_dim[0][0] = point.x;
 			}
-			if (point.y < min_max_dim[0][1]){
-				min_max_dim[1][1] = point.y;
+			if (point.y < min_max_dim[0][1]) {
+				min_max_dim[0][1] = point.y;
 			}
-			if (point.z < min_max_dim[0][2]){
-				min_max_dim[1][2] = point.z;
+			if (point.z < min_max_dim[0][2]) {
+				min_max_dim[0][2] = point.z;
 			}
 
-			if (point.x > min_max_dim[1][0]){
+			if (point.x > min_max_dim[1][0]) {
 				min_max_dim[1][0] = point.x;
 			}
-			if (point.y > min_max_dim[1][1]){
+			if (point.y > min_max_dim[1][1]) {
 				min_max_dim[1][1] = point.y;
 			}
-			if (point.z > min_max_dim[1][2]){
+			if (point.z > min_max_dim[1][2]) {
 				min_max_dim[1][2] = point.z;
 			}
 		}
-        return min_max_dim;
-    }
+		return min_max_dim;
+	}
 
-    public static double findLength(List<Point_3> points){
-		double[] min_dim  = min_and_max_dim(points)[0];
-		double[] max_dim  = min_and_max_dim(points)[1];
-        double widthX = max_dim[0] - min_dim[0];
+	public static double findLength(List<Point_3> points) {
+		double[] min_dim = min_and_max_dim(points)[0];
+		double[] max_dim = min_and_max_dim(points)[1];
+		double widthX = max_dim[0] - min_dim[0];
 		double widthY = max_dim[1] - min_dim[1];
 		double widthZ = max_dim[2] - min_dim[2];
-        return(Math.max(Math.max(widthY, widthZ), widthX));
-    }
-    
-    public static Point_3 findCenter(List<Point_3> points){
-		double[] min_dim  = min_and_max_dim(points)[0];
-		double[] max_dim  = min_and_max_dim(points)[1];
-        Point_3 center = new Point_3((max_dim[0] + min_dim[0])/2, (max_dim[1] + min_dim[1])/2, (max_dim[2] + min_dim[2])/2);
-        return center;
-    }
-		
-	
+		return (Math.max(Math.max(widthY, widthZ), widthX));
+	}
+
+	public static Point_3 findCenter(List<Point_3> points) {
+		double[] min_dim = min_and_max_dim(points)[0];
+		double[] max_dim = min_and_max_dim(points)[1];
+		Point_3 center = new Point_3((max_dim[0] + min_dim[0]) / 2, (max_dim[1] + min_dim[1]) / 2,
+				(max_dim[2] + min_dim[2]) / 2);
+		return center;
+	}
+
 }
